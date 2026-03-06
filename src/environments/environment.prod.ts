cfaadb84b1e07012745a398e467f38b44a92c128
Enrico Nascimento SIlva <enriconascimento9@gmail.com>
2026-03-06 16:04:35 -0300

    chore(firebase): add firebase dependency and environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB5O6D5oLp9k0fvgabFIvZgrfHnlGbrQaU',
+    authDomain: 'ionic-showcase-e025f.firebaseapp.com',
+    projectId: 'ionic-showcase-e025f',
+    storageBucket: 'ionic-showcase-e025f.firebasestorage.app',
+    messagingSenderId: '208067385326',
+    appId: '1:208067385326:web:b54ec344ca011fe028e0ff',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB5O6D5oLp9k0fvgabFIvZgrfHnlGbrQaU',
+    authDomain: 'ionic-showcase-e025f.firebaseapp.com',
+    projectId: 'ionic-showcase-e025f',
+    storageBucket: 'ionic-showcase-e025f.firebasestorage.app',
+    messagingSenderId: '208067385326',
+    appId: '1:208067385326:web:b54ec344ca011fe028e0ff',
+  },
 };
 
 /*
