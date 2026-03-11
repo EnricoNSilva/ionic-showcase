69471c17c71a6c16c038623da4fe403644e1598f
Enrico Nascimento SIlva <enriconascimento9@gmail.com>
2026-03-11 09:50:23 -0300

    chore(firebase): add dependency and update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,24 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () =>
+      import('./cadastro/cadastro.page').then((m) => m.CadastroPage),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
   },
 ];
